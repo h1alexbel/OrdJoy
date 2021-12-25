8f8b3af4db1104f9c36b293d6c93d0c34c8f3ba4
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2021-12-25 16:42:34 +0300

    update script.sql

--- resources/database/script.sql
+++ resources/database/script.sql
@@ -7,10 +7,10 @@ CREATE SCHEMA audio_tracks_storage;
 CREATE TABLE user_storage.user_account
 (
     id       SERIAL PRIMARY KEY,
-    email    CHARACTER VARYING(64) UNIQUE NOT NULL,
-    login    CHARACTER VARYING(128)       NOT NULL UNIQUE,
-    password CHARACTER VARYING(128)       NOT NULL,
-    role     CHARACTER VARYING CHECK (role = 'Admin' OR role = 'Client' OR role = 'admin' OR role = 'client')
+    email    CHARACTER VARYING(64) UNIQUE                                                                     NOT NULL,
+    login    CHARACTER VARYING(128)                                                                           NOT NULL UNIQUE,
+    password CHARACTER VARYING(128)                                                                           NOT NULL,
+    role     CHARACTER VARYING CHECK (role = 'Admin' OR role = 'Client' OR role = 'admin' OR role = 'client') NOT NULL
 );
 
 CREATE TABLE user_storage.user_data
@@ -21,6 +21,9 @@ CREATE TABLE user_storage.user_data
     age             INT                   NOT NULL,
     user_account_id INT REFERENCES user_storage.user_account (id)
 );
+
+ALTER TABLE user_storage.user_account
+    ADD CONSTRAINT user_data_id FOREIGN KEY (id) REFERENCES user_storage.user_data;
 
 CREATE TABLE audio_tracks_storage.genre
 (
@@ -35,13 +38,22 @@ CREATE TABLE audio_tracks_storage.album
     genre_id INT REFERENCES audio_tracks_storage.genre (id)
 );
 
+CREATE TABLE audio_tracks_storage.mix
+(
+    id          SERIAL PRIMARY KEY,
+    name        CHARACTER VARYING(64) UNIQUE NOT NULL,
+    description TEXT                         NOT NULL,
+    genre_id    INT REFERENCES audio_tracks_storage.genre (id)
+);
+
 CREATE TABLE audio_tracks_storage.track
 (
     id       SERIAL PRIMARY KEY,
     song_url TEXT NOT NULL UNIQUE,
     title    TEXT NOT NULL,
     genre_id INT REFERENCES audio_tracks_storage.genre (id),
-    album_id INT REFERENCES audio_tracks_storage.album (id)
+    album_id INT REFERENCES audio_tracks_storage.album (id),
+    mix_id   INT REFERENCES audio_tracks_storage.mix (id)
 );
 
 CREATE TABLE user_storage.order
@@ -67,11 +79,9 @@ CREATE TABLE audio_tracks_storage.artist_tracks
     PRIMARY KEY (artist_id, track_id)
 );
 
-CREATE TABLE audio_tracks_storage.mix
+CREATE TABLE audio_tracks_storage.track_mixes
 (
-    id          SERIAL PRIMARY KEY,
-    name        CHARACTER VARYING(64) UNIQUE NOT NULL,
-    description TEXT                         NOT NULL,
-    genre_id    INT REFERENCES audio_tracks_storage.genre (id),
-    track_id    INT REFERENCES audio_tracks_storage.track (id)
+    track_id INT REFERENCES audio_tracks_storage.track (id),
+    mix_id   INT REFERENCES audio_tracks_storage.mix (id),
+    PRIMARY KEY (track_id, mix_id)
 );
