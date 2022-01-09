f3c8d9467f63c53e8927d73807bfc298ab6bdcf7
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-01-09 09:28:52 +0300

    fix problems in script.sql 1) reduce count of tables 2) change datatype TEXT on VAR CHAR(512) 3) delete genre table 4) add review table 5) add in table user_account_data field discount_percentage_level

--- resources/database/script.sql
+++ resources/database/script.sql
@@ -4,29 +4,18 @@ CREATE SCHEMA user_storage;
 
 CREATE SCHEMA audio_tracks_storage;
 
-CREATE TABLE user_storage.user_account
+CREATE TABLE user_storage.user_account_data
 (
-    id       BIGSERIAL PRIMARY KEY,
-    email    CHARACTER VARYING(64) UNIQUE  NOT NULL,
-    login    CHARACTER VARYING(128) UNIQUE NOT NULL,
-    password CHARACTER VARYING(128)        NOT NULL,
-    role     CHARACTER VARYING             NOT NULL
-);
-
-CREATE TABLE user_storage.user_data
-(
-
-    first_name      CHARACTER VARYING(64) NOT NULL,
-    last_name       CHARACTER VARYING(64) NOT NULL,
-    age             INT                   NOT NULL,
-    user_account_id BIGINT REFERENCES user_storage.user_account (id),
-    PRIMARY KEY (user_account_id)
-);
-
-CREATE TABLE audio_tracks_storage.genre
-(
-    id   BIGSERIAL PRIMARY KEY,
-    name CHARACTER VARYING(64) UNIQUE
+    id                        BIGSERIAL PRIMARY KEY,
+    email                     CHARACTER VARYING(64) UNIQUE  NOT NULL,
+    login                     CHARACTER VARYING(128) UNIQUE NOT NULL,
+    password                  CHARACTER VARYING(128)        NOT NULL,
+    discount_percentage_level INT                           NOT NULL,
+    role                      CHARACTER VARYING             NOT NULL,
+    first_name                CHARACTER VARYING(64)         NOT NULL,
+    last_name                 CHARACTER VARYING(64)         NOT NULL,
+    age                       INT                           NOT NULL,
+    card_number               CHARACTER VARYING(32)         NOT NULL
 );
 
 CREATE TABLE audio_tracks_storage.artist
@@ -35,68 +24,48 @@ CREATE TABLE audio_tracks_storage.artist
     name CHARACTER VARYING(64) UNIQUE NOT NULL
 );
 
-CREATE TABLE audio_tracks_storage.album
-(
-    id        BIGSERIAL PRIMARY KEY,
-    title     CHARACTER VARYING(128) NOT NULL,
-    genre_id  BIGINT REFERENCES audio_tracks_storage.genre (id),
-    artist_id BIGINT REFERENCES audio_tracks_storage.artist (id)
-);
-
 CREATE TABLE audio_tracks_storage.mix
 (
     id          BIGSERIAL PRIMARY KEY,
     name        CHARACTER VARYING(64) UNIQUE NOT NULL,
-    description TEXT                         NOT NULL,
-    genre_id    BIGINT REFERENCES audio_tracks_storage.genre (id)
+    description CHARACTER VARYING(512)       NOT NULL
+);
+
+CREATE TABLE audio_tracks_storage.album
+(
+    id        BIGSERIAL PRIMARY KEY,
+    title     CHARACTER VARYING(128) NOT NULL,
+    artist_id BIGINT REFERENCES audio_tracks_storage.artist (id)
 );
 
 CREATE TABLE audio_tracks_storage.track
 (
     id       BIGSERIAL PRIMARY KEY,
-    song_url TEXT NOT NULL UNIQUE,
-    title    TEXT NOT NULL,
-    genre_id BIGINT REFERENCES audio_tracks_storage.genre (id) ON DELETE CASCADE,
-    album_id BIGINT REFERENCES audio_tracks_storage.album (id) ON DELETE CASCADE
+    song_url CHARACTER VARYING(512) NOT NULL UNIQUE,
+    title    CHARACTER VARYING(512) NOT NULL,
+    album_id BIGINT REFERENCES audio_tracks_storage.album (id)
 );
 
 CREATE TABLE user_storage.order
 (
 
     id              BIGSERIAL PRIMARY KEY,
-    price           NUMERIC               NOT NULL,
-    card_number     CHARACTER VARYING(32) NOT NULL,
-    user_account_id BIGINT REFERENCES user_storage.user_account (id),
+    price           NUMERIC NOT NULL,
+    user_account_id BIGINT REFERENCES user_storage.user_account_data (id),
     track_id        BIGINT REFERENCES audio_tracks_storage.track (id)
 );
 
-CREATE TABLE audio_tracks_storage.artist_tracks
+CREATE TABLE user_storage.review
 (
-    artist_id BIGINT REFERENCES audio_tracks_storage.artist (id) ON DELETE CASCADE,
-    track_id  BIGINT REFERENCES audio_tracks_storage.track (id) ON DELETE CASCADE,
-    PRIMARY KEY (artist_id, track_id)
+    id              BIGSERIAL PRIMARY KEY,
+    review_text     CHARACTER VARYING(512) NOT NULL,
+    user_account_id BIGINT REFERENCES user_storage.user_account_data (id),
+    track_id        BIGINT REFERENCES audio_tracks_storage.track (id)
 );
 
 CREATE TABLE audio_tracks_storage.track_mixes
 (
-    track_id BIGINT REFERENCES audio_tracks_storage.track (id) ON DELETE CASCADE,
-    mix_id   BIGINT REFERENCES audio_tracks_storage.mix (id) ON DELETE CASCADE,
+    track_id BIGINT REFERENCES audio_tracks_storage.track (id),
+    mix_id   BIGINT REFERENCES audio_tracks_storage.mix (id),
     PRIMARY KEY (track_id, mix_id)
 );
-
-INSERT INTO audio_tracks_storage.genre (name)
-VALUES ('POP'),
-       ('ELECTRONIC'),
-       ('RAP'),
-       ('RB'),
-       ('LATIN'),
-       ('ROCK'),
-       ('METAL'),
-       ('COUNTRY'),
-       ('FOLK'),
-       ('JAZZ'),
-       ('CLASSICAL'),
-       ('NEW_AGE'),
-       ('BLUES'),
-       ('TRADITIONAL'),
-       ('EASY_LISTENING');
