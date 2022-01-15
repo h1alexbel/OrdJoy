13a37cfddf0293b7aab728869de4e30f834ec47a
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-01-15 21:34:20 +0300

    add MixService

---
+++ src/com/ordjoy/service/MixService.java
@@ -0,0 +1,87 @@
+package com.ordjoy.service;
+
+import com.ordjoy.dao.filter.DefaultFilter;
+import com.ordjoy.dao.filter.MixFilter;
+import com.ordjoy.dao.impl.MixDaoImpl;
+import com.ordjoy.dto.MixDto;
+import com.ordjoy.dto.MixReviewDto;
+import com.ordjoy.entity.Mix;
+
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.*;
+
+public class MixService {
+
+    private final MixDaoImpl mixDao = MixDaoImpl.getInstance();
+
+    private static final MixService INSTANCE = new MixService();
+
+    private MixService() {
+
+    }
+
+    public static MixService getInstance() {
+        return INSTANCE;
+    }
+
+    public Mix saveMix(Mix mix) {
+        return mixDao.save(mix);
+    }
+
+    public Optional<MixDto> findMixById(Long id) {
+        return mixDao.findById(id).stream()
+                .map(mix -> new MixDto(
+                        mix.getId(),
+                        mix.getName(),
+                        mix.getDescription()
+                )).findFirst();
+    }
+
+    public List<MixDto> findAllMixes(MixFilter filter) {
+        return mixDao.findAll(filter).stream()
+                .map(mix -> new MixDto(
+                        mix.getId(),
+                        mix.getName(),
+                        mix.getDescription()
+                )).collect(toList());
+    }
+
+    public void updateMix(Mix mix) {
+        mixDao.update(mix);
+    }
+
+    public boolean deleteMixById(Long id) {
+        return mixDao.deleteById(id);
+    }
+
+    public Optional<MixDto> findMixByMixName(String mixName) {
+        return mixDao.findMixByMixName(mixName).stream()
+                .map(mix -> new MixDto(
+                        mix.getId(),
+                        mix.getName(),
+                        mix.getDescription()
+                )).findFirst();
+    }
+
+    public List<MixReviewDto> findMixReviewByMixName(String mixName, DefaultFilter filter) {
+        return mixDao.findMixReviewByMixName(mixName, filter).stream()
+                .map(mixReview -> new MixReviewDto(
+                        mixReview.getId(),
+                        mixReview.getReviewText(),
+                        mixReview.getMix().getName(),
+                        mixReview.getUserAccount().getLogin()
+                )).collect(toList());
+    }
+
+    public List<MixReviewDto> findMixReviewsByMixId(Long mixId, DefaultFilter filter) {
+        return mixDao.findMixReviewsByMixId(mixId, filter).stream()
+                .map(mixReview -> new MixReviewDto(
+                        mixReview.getId(),
+                        mixReview.getReviewText(),
+                        mixReview.getMix().getName(),
+                        mixReview.getUserAccount().getLogin()
+                )).collect(toList());
+    }
+}
