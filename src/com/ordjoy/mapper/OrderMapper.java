874f06b71205c2582d406323f648729b24cbb7b8
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-01-16 19:39:23 +0300

    add mapper package

---
+++ src/com/ordjoy/mapper/OrderMapper.java
@@ -0,0 +1,27 @@
+package com.ordjoy.mapper;
+
+import com.ordjoy.dto.OrderDto;
+import com.ordjoy.entity.Order;
+
+public class OrderMapper implements Mapper<Order, OrderDto> {
+
+    private static final OrderMapper INSTANCE = new OrderMapper();
+
+    private OrderMapper() {
+
+    }
+
+    public static OrderMapper getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public OrderDto mapFrom(Order order) {
+        return new OrderDto(
+                order.getId(),
+                order.getPrice(),
+                UserAccountMapper.getInstance().mapFrom(order.getUserAccount()),
+                TrackMapper.getInstance().mapFrom(order.getTrack())
+        );
+    }
+}
