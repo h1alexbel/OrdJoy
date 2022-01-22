41aa41c6b59b99c72a7079c541e36c54f36ee07a
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-01-22 21:15:18 +0300

    add MainController

---
+++ src/com/ordjoy/controller/MainServlet.java
@@ -0,0 +1,56 @@
+package com.ordjoy.controller;
+
+import com.ordjoy.command.FrontCommand;
+import com.ordjoy.command.FrontCommandFactory;
+import com.ordjoy.command.FrontCommandResult;
+import com.ordjoy.dbmanager.ConnectionPool;
+import com.ordjoy.exception.ControllerException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/frontController")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        ConnectionPool.getInstance();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processFrontCommand(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processFrontCommand(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        ConnectionPool.getInstance().closeConnectionPool();
+    }
+
+    private void processFrontCommand(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws ServletException, IOException {
+        String frontCommand = httpServletRequest.getParameter("frontCommand");
+        FrontCommand command = FrontCommandFactory.getCommand(frontCommand);
+        try {
+            FrontCommandResult result = command.process(httpServletRequest);
+            switch (result.getNavigationType()) {
+                case FORWARD -> getServletContext().getRequestDispatcher(result.getPage()).forward(httpServletRequest, httpServletResponse);
+                case REDIRECT -> httpServletResponse.sendRedirect(result.getPage());
+                default -> throw new IllegalStateException("Unexpected value: " + result.getNavigationType());
+            }
+        } catch (ControllerException e) {
+            throw new ServletException(e);
+        }
+    }
+}
