4d9b36623627d4c16e15d6e6914c5dece2b2744d
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-02-02 13:24:17 +0300

    add commands to delete, decline, do order

---
+++ src/com/ordjoy/command/impl/DeleteOrderCommand.java
@@ -0,0 +1,41 @@
+package com.ordjoy.command.impl;
+
+import com.ordjoy.command.FrontCommand;
+import com.ordjoy.command.FrontCommandResult;
+import com.ordjoy.command.NavigationType;
+import com.ordjoy.exception.ControllerException;
+import com.ordjoy.exception.ServiceException;
+import com.ordjoy.service.OrderService;
+import com.ordjoy.util.ExceptionMessageUtils;
+import com.ordjoy.util.JspFormatHelper;
+import com.ordjoy.util.JspPageConst;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.ordjoy.util.ExceptionMessageUtils.*;
+import static com.ordjoy.util.JspPageConst.*;
+
+public class DeleteOrderCommand implements FrontCommand {
+
+    private static final String ORDER_ID = "orderId";
+    private final OrderService orderService = OrderService.getInstance();
+
+    @Override
+    public FrontCommandResult process(HttpServletRequest httpServletRequest) throws ControllerException {
+        String page;
+        FrontCommandResult frontCommandResult;
+        Long id = Long.valueOf(httpServletRequest.getParameter(ORDER_ID));
+        try {
+            boolean isDeleted = orderService.deleteOrderById(id);
+            if (isDeleted) {
+                page = httpServletRequest.getHeader(REFERER_HEADER);
+            } else {
+                page = httpServletRequest.getContextPath() + JspFormatHelper.getAdminPath(ADMIN_DASHBOARD_PAGE);
+            }
+            frontCommandResult = new FrontCommandResult(page, NavigationType.REDIRECT);
+            return frontCommandResult;
+        } catch (ServiceException e) {
+            throw new ControllerException(CONTROLLER_EXCEPTION_MESSAGE, e);
+        }
+    }
+}
