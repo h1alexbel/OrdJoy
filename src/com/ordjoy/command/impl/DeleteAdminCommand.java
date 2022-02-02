33eb312bd713164187f69187dd90c65d1517d0c4
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-02-02 13:36:35 +0300

    add DeleteAdminCommand

---
+++ src/com/ordjoy/command/impl/DeleteAdminCommand.java
@@ -0,0 +1,39 @@
+package com.ordjoy.command.impl;
+
+import com.ordjoy.command.FrontCommand;
+import com.ordjoy.command.FrontCommandResult;
+import com.ordjoy.command.NavigationType;
+import com.ordjoy.exception.ControllerException;
+import com.ordjoy.exception.ServiceException;
+import com.ordjoy.service.UserService;
+import com.ordjoy.util.JspFormatHelper;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.ordjoy.util.ExceptionMessageUtils.CONTROLLER_EXCEPTION_MESSAGE;
+import static com.ordjoy.util.JspPageConst.*;
+
+public class DeleteAdminCommand implements FrontCommand {
+
+    private static final String USER_ID = "userId";
+    private final UserService userService = UserService.getInstance();
+
+    @Override
+    public FrontCommandResult process(HttpServletRequest httpServletRequest) throws ControllerException {
+        String page;
+        FrontCommandResult frontCommandResult;
+        Long id = Long.valueOf(httpServletRequest.getParameter(USER_ID));
+        try {
+            boolean isDeleted = userService.deleteUserById(id);
+            if (isDeleted) {
+                page = httpServletRequest.getHeader(REFERER_HEADER);
+            } else {
+                page = httpServletRequest.getContextPath() + JspFormatHelper.getAdminPath(ADMIN_DASHBOARD_PAGE);
+            }
+            frontCommandResult = new FrontCommandResult(page, NavigationType.REDIRECT);
+            return frontCommandResult;
+        } catch (ServiceException e) {
+            throw new ControllerException(CONTROLLER_EXCEPTION_MESSAGE, e);
+        }
+    }
+}
