ee58c461f7fee7e688e4bcf211565b9e5dd13de4
alexeybel <83453843+h1alexbel@users.noreply.github.com>
2022-02-12 21:24:12 +0300

    fix PasswordEncoderTest

--- test/com/ordjoy/util/PasswordEncoderTest.java
+++ test/com/ordjoy/util/PasswordEncoderTest.java
@@ -1,14 +1,12 @@
 package com.ordjoy.util;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PasswordEncoderTest {
 
     @Test
-    @ParameterizedTest
     public void encodingTest() {
         String password = "qwerty12345";
         String encodedPassword = PasswordEncoder.encode(password);
